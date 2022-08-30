2125a9aa88f63390d36425fabc0b2d1c049c59de
KawaiiNee <kasuganosora002@gmail.com>
2022-08-30 21:24:58 +0800

    .

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, end) => {
+  alert(err);
   console.log(err);
   let customError = {
     statusCode: err.sttatusCode || StatusCodes.INTERNAL_SERVER_ERROR,
